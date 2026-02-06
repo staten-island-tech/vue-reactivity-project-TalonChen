302f393963a7d1cc05fd1c2bbad40a4cc6d33a61
TalonChen <talonc@nycstudents.net>
2026-02-06 13:46:15 -0500

    d

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import MusicCreate from `@/views/MusicCreate.vue`
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: null,
+      component: MusicCreate,
     },
   ],
 })
