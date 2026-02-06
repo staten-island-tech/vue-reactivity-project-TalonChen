05dd750485ca1bb6e218e10aedb8948d92db73a1
TalonChen <talonc@nycstudents.net>
2026-02-06 14:01:32 -0500

    errr

--- app/src/views/MusicCreate.vue
+++ app/src/views/MusicCreate.vue
@@ -5,7 +5,30 @@
 </template>
 
 <script setup>
-const keys = {}
+import { ref, reactive } from 'vue'
+const keys = ref([
+  { name: 'A', pitch: 'high' },
+  { name: 'A', pitch: 'normal' },
+  { name: 'A', pitch: 'low' },
+  { name: 'B', pitch: 'high' },
+  { name: 'B', pitch: 'normal' },
+  { name: 'B', pitch: 'low' },
+  { name: 'C', pitch: 'high' },
+  { name: 'C', pitch: 'normal' },
+  { name: 'C', pitch: 'low' },
+  { name: 'D', pitch: 'high' },
+  { name: 'D', pitch: 'normal' },
+  { name: 'D', pitch: 'low' },
+  { name: 'E', pitch: 'high' },
+  { name: 'E', pitch: 'normal' },
+  { name: 'E', pitch: 'low' },
+  { name: 'F', pitch: 'high' },
+  { name: 'F', pitch: 'normal' },
+  { name: 'F', pitch: 'low' },
+  { name: 'G', pitch: 'high' },
+  { name: 'G', pitch: 'normal' },
+  { name: 'G', pitch: 'low' },
+])
 </script>
 
 <style scoped></style>
