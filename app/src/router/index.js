edd29a3e6b08bb7bfcd6378436378b4a0aec9e07
TalonChen <talonc@nycstudents.net>
2026-02-09 13:59:45 -0500

    idea change

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MusicCreate from '@/views/MusicCreate.vue'
+import AnimalList from '@/views/AnimalList.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: MusicCreate,
+      component: AnimalList,
     },
   ],
 })
---
+++ app/src/views/AnimalList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>haiioi</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+const AnimalParts = ref([
+  { animal: Lion, Part: Tail },
+  { animal: Lion, Part: Head },
+  { animal: Lion, Part: RightLegs },
+  { animal: Lion, Part: LeftLegs },
+  { animal: Lion, Part: MainBody },
+  { animal: Tiger, Part: Tail },
+  { animal: Tiger, Part: Head },
+  { animal: Tiger, Part: RightLegs },
+  { animal: Tiger, Part: LeftLegs },
+  { animal: Tiger, Part: MainBody },
+  { animal: Cat, Part: Tail },
+  { animal: Cat, Part: Head },
+  { animal: Cat, Part: RightLegs },
+  { animal: Cat, Part: LeftLegs },
+  { animal: Cat, Part: MainBody },
+  { animal: Dog, Part: Tail },
+  { animal: Dog, Part: Head },
+  { animal: Dog, Part: RightLegs },
+  { animal: Dog, Part: LeftLegs },
+  { animal: Dog, Part: MainBody },
+])
+</script>
+
+<style scoped></style>
